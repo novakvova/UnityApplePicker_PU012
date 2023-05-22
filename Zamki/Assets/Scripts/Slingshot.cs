e7a74de5e6bd1798896dec8ecc59cbfd9b990519
novakvova <novakvova@gmail>
2023-05-22 10:11:10 +0300

    Add new Game Zamki

---
+++ Zamki/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    [Header("Set in Inpector")]
+    public GameObject prefabProjectile;
+    public float velocityMult = 8;
+
+    [Header("Set Dynamicaly")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+    private Rigidbody projectileRigidbody;
+
+
+    private void Awake()
+    {
+        Transform launchPointTransform = transform.Find("LaunchPoint");
+        launchPoint = launchPointTransform.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTransform.position;
+    }
+    private void OnMouseEnter()
+    {
+        //print("Мишка в області колайдера");
+        launchPoint.SetActive(true);
+    }
+    private void OnMouseExit()
+    {
+        //print("Забрали кнопку мишки");
+        launchPoint.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile);
+        projectile.transform.position = launchPos;
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
+    }
+    private void Update()
+    {
+        if(!aimingMode) return;
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if(mouseDelta.magnitude>maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if(Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
+            projectile = null;
+        }
+    }
+}
